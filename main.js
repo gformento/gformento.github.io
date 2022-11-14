9df835e577391b489a71350247c58519391db878
Gonzalo Formento <94689353+gformento@users.noreply.github.com>
2022-11-14 12:51:07 -0300

    first commit

--- main.js
+++ main.js
@@ -2,16 +2,23 @@
 //Seleccionar los elementos HTML que necesito
 
 var input = document.getElementById("tarea");
-var boton = document.getElementById("btn")
-var tareas = document.querySelector("ul")
+var boton = document.getElementById("btn");
+var tareas = document.querySelector("ul");
+
 //Cuando se haga click en el boton
 
 boton.addEventListener("click",function(){
+
   //Obtener lo que hay escrito  en el <input>
   var tarea = input.value
   
   //Añadirlo como un nuevo elemento <li>
+
   var li = document.createElement("li")
+  li.textContent = tarea
+  tareas.appendChild(li)
+
+  var deleteLi = document.deleteElement("li")
   li.textContent = tarea
   tareas.appendChild(li)
 })
