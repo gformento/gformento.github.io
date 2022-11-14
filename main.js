75e5a75b61cf493cee4005155003fb7054a877da
Gonzalo Formento <94689353+gformento@users.noreply.github.com>
2022-11-14 09:54:40 -0300

    Pagina Prueba

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-function(formulario){
-    
-};
+
+//Seleccionar los elementos HTML que necesito
+
+var input = document.getElementById("tarea");
+var boton = document.getElementById("btn")
+var tareas = document.querySelector("ul")
+//Cuando se haga click en el boton
+
+boton.addEventListener("click",function(){
+  //Obtener lo que hay escrito  en el <input>
+  var tarea = input.value
+  
+  //Añadirlo como un nuevo elemento <li>
+  var li = document.createElement("li")
+  li.textContent = tarea
+  tareas.appendChild(li)
+})
